1d3b50aaee94da85ea847b7de5e6a77e3224de06
neriyarosner <neriyarosner@gmail.com>
2018-11-01 17:34:52 +0200

    add class map

--- chatWSServer.php
+++ chatWSServer.php
@@ -1,10 +1,6 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 use Inc\Libs\Configurator;
 use Inc\Libs\Output;
 
